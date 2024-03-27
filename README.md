23082fca71a6815c67cc58f438f7008d21d78e96
abikouo <abikouo@redhat.com>
2024-03-27 12:50:13 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# ansible-ee-testing
-This repository describes how to automate testing of Ansible Execution environment
+# Testing cloud collection Using Execution environment
+
+
+## How to build execution environment
+
+In this context we are going to test AAP 2.5 using execution environment base image ``registry-proxy.engineering.redhat.com/rh-osbs/ansible-automation-platform-25-ee-supported-rhel9:latest``.
+From RedHat VPN run the following command:
+
+```shell
+docker pull registry-proxy.engineering.redhat.com/rh-osbs/ansible-automation-platform-25-ee-supported-rhel9:latest
+```
+
+Then build execution environment using the following command:
+
+```shell
+ansible-builder build -f ./execution-environment.yml --container-runtime docker -t <image_tag_name> --no-cache -v 3
+```
+
+
+here the content of my execution environment file
+
+```yaml
+---
+version: 3
+
+dependencies:
+  galaxy: requirements.yml
+  python: requirements.txt
+  system: bindep.txt
+  ansible_core:
+    package_pip: ansible-core==2.16.4
+  ansible_runner:
+    package_pip: ansible-runner==2.3.6
+  python_interpreter:
+    package_system: "python3.11"
+    python_path: "/usr/bin/python3.11"
+
+images:
+  base_image:
+    name: registry-proxy.engineering.redhat.com/rh-osbs/ansible-automation-platform-25-ee-supported-rhel9:latest
+
+options:
+  package_manager_path: /usr/bin/microdnf
+
+additional_build_files:
+  - src: ansible.cfg
+    dest: configs
+
+additional_build_steps:
+  append_base: |
+    RUN echo "Target architecture is: $TARGETARCH"
+
+    # Configure Terraform Repo
+    RUN curl -o /etc/yum.repos.d/hashicorp.repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+
+  prepend_galaxy: |
+    COPY _build/configs/ansible.cfg /etc/ansible/ansible.cfg
+    ENV ANSIBLE_GALAXY_SERVER_AUTOMATION_HUB_URL=https://console.redhat.com/api/automation-hub/content/published/
+    ENV ANSIBLE_GALAXY_SERVER_AUTOMATION_HUB_AUTH_URL=https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token
+    ARG ANSIBLE_GALAXY_SERVER_AUTOMATION_HUB_TOKEN
+
+  append_final: |
+    # Use Python 3.11 as default
+    RUN rm -f /bin/python3
+    RUN ln -s /bin/python3.11 /bin/python3
+
+    # Output collections list for debugging
+    RUN ansible-galaxy collection list
+
+    # Display terraform version
+    RUN terraform version
+```
+
+
+## How to run integration test targets
+
+I first set the collection version tag to the expected version.
+For example, for ``amazon.aws`` collection, run the following:
+
+```shell
+cd <path to collection> && git checkout x.x.x
+```
+
+Then run the following:
+
+```shell
+# the tool is forcing ansible-navigator to use docker as container runtime, this can be easily updated
+python ./run.py --eei <image_tag_name> --collection-path ~/.ansible/collections/ansible_collections/amazon/aws --vars-file vars.yaml
+```
+
+with ``vars.yaml``
+
+```yaml
+output_dir: "/tmp"
+aws_access_key: xxxx # aws access key
+aws_secret_key: xxxx # aws secret key
+aws_region: us-east-1
+```
---
+++ run.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python
+"""Script to request new aws session using ansible_core_ci_key."""
+
+import copy
+import os
+import platform
+import random
+import subprocess
+from argparse import ArgumentParser
+from pathlib import PosixPath
+from tempfile import NamedTemporaryFile
+import re
+import yaml
+
+
+def generate_resource_prefix():
+    prefix = "ansible-test-%d-%s" % (
+        random.randint(10000000, 99999999),
+        platform.node().split(".")[0],
+    )
+
+    return prefix
+
+
+def construct_playbook(yaml_file, role_name, variables):
+
+    tasks = [
+        {
+            "name": "Create temporary directory to run test.",
+            "register": "tmp_path",
+            "ansible.builtin.tempfile":{
+                "suffix": ".tf", "state": "directory"
+            }
+        },
+        {
+            "name": f"Execute ansible role '{role_name}'",
+            "vars": {"output_dir": "{{ tmp_path.path }}"},
+            "block": [
+                {
+                    "ansible.builtin.include_role": {
+                        "name": role_name
+                    }
+                }
+            ],
+            "always":[
+                {
+                    "name": "Delete temporary directory",
+                    "ansible.builtin.file": {
+                        "state": "absent",
+                        "path": "{{ tmp_path.path }}"
+                    }
+                }
+            ]
+        },
+    ]
+
+    data = {
+        "hosts": "localhost",
+        "gather_facts": False,
+        "vars": variables,
+        "tasks": tasks,
+    }
+    yaml.dump([data], yaml_file, default_flow_style=False, explicit_start=True)
+
+
+def skip_target(path, targets) -> bool:
+    
+    if targets and path.stem not in targets:
+        return True
+    
+    runme = path / PosixPath("runme.sh")
+    if runme.exists():
+        print("[SKIP] ", path.stem, " contains 'runme.sh' file.")
+        return True
+
+    aliases = path / PosixPath("aliases")
+    if not aliases.exists():
+        return True
+
+    attributes = aliases.read_text().split("\n")
+    if any(x.startswith("disabled") for x in attributes) or any(x.startswith("unsupported") for x in attributes):
+        print("[SKIP] ", path.stem, " disabled or unsupported")
+        return True 
+
+    for x in attributes:
+        m = re.match('^time=([0-9]*)m', x)
+        if m:
+            if int(m.group(1)) > 5:
+                print("[SKIP] ", path.stem, " too long, estimated time = ", m.group(1), "min")
+                return True
+    return False
+
+
+def run(eei_image, collection_path, vars_file, targets, use_stdout):
+    variables = {}
+    if vars_file:
+        with open(vars_file, "r") as fd:
+            variables.update(yaml.safe_load(fd))
+    print("Variables => ", variables)
+
+    if targets:
+        targets = [t.strip() for t in targets.split(",") if t]
+
+    # Build AWS credentials variables
+    set_envs = ["--senv ANSIBLE_ROLES_PATH=/roles"]
+    aws_cred_mapping = {
+        "aws_access_key": "AWS_ACCESS_KEY_ID",
+        "aws_secret_key": "AWS_SECRET_ACCESS_KEY",
+        "security_token": "AWS_SESSION_TOKEN",
+        "aws_region": "AWS_REGION",
+    }
+    for k, v in aws_cred_mapping.items():
+        u_value = variables.get(k)
+        if u_value:
+            set_envs.append(f"--senv '{v}={u_value}'")
+
+    path = PosixPath(collection_path) / PosixPath("tests/integration/targets")
+    results = []
+    for target in path.iterdir():
+
+        if skip_target(target, targets):
+            continue
+
+        yaml_file = NamedTemporaryFile(mode="w", suffix=".yaml", delete=False)
+        test_variables = copy.deepcopy(variables)
+        test_variables.update({"resource_prefix": generate_resource_prefix()})
+        construct_playbook(yaml_file, target.stem, test_variables)
+        yaml_file.close()
+
+        command = [
+            "ansible-navigator",
+            "run",
+            yaml_file.name,
+            "-v",
+            "--ee true",
+            f"--eei {eei_image}",
+            "--ce docker",
+            f"--eev {path.expanduser()}:/roles:Z",
+            "-m stdout",
+        ] + set_envs
+        print("\033[93m++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print(f"+    RUNNING TARGET => {target.stem}")
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++\033[00m")
+        if use_stdout:
+            cmd = subprocess.Popen(" ".join(command), shell=True)
+        else:
+            cmd = subprocess.Popen(" ".join(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        stdout, stderr = cmd.communicate()
+        os.remove(yaml_file.name)
+        result = f"++++ {target.stem} -> "
+        if cmd.returncode == 0:
+            result += "\033[92mOK\033[00m"
+        else:
+            if use_stdout:
+                result += f"\033[91mKO\033[00m"
+            else:
+                stdout_f = f"{target.stem}_stdout.txt"
+                with open(stdout_f, "wb") as f:
+                    f.write(stdout)
+                result += f"\033[91mKO\033[00m (See details {stdout_f})"
+        if use_stdout:
+            results.append(result)
+        else:
+            print(result)
+    print("\n".join(results))
+
+
+def main() -> None:
+    parser = ArgumentParser(
+        description="Generate playbook and run using execution environment."
+    )
+    parser.add_argument("--eei", required=True, help="Execution environment image.")
+    parser.add_argument(
+        "--collection-path", required=True, help="Path to the collection to test."
+    )
+    parser.add_argument(
+        "--vars-file", help="Path to a file containing variables to set to run tests."
+    )
+    parser.add_argument("--targets", help="Comma-separated list of targets to test.")
+    parser.add_argument("--use-stdout", action='store_true', help="Use stdout for the execution output")
+
+    args = parser.parse_args()
+
+    run(
+        eei_image=args.eei,
+        collection_path=args.collection_path,
+        vars_file=args.vars_file,
+        targets=args.targets,
+        use_stdout=args.use_stdout,
+    )
+
+
+if __name__ == "__main__":
+    main()
