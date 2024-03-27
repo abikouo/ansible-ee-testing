4b97338744f945e325ebc12468039be7063d612c
abikouo <abikouo@redhat.com>
2024-03-27 17:15:14 +0100

    some new updates

--- run.py
+++ run.py
@@ -5,11 +5,13 @@ import copy
 import os
 import platform
 import random
+import re
 import subprocess
+import uuid
 from argparse import ArgumentParser
 from pathlib import PosixPath
 from tempfile import NamedTemporaryFile
-import re
+
 import yaml
 
 
@@ -22,35 +24,30 @@ def generate_resource_prefix():
     return prefix
 
 
-def construct_playbook(yaml_file, role_name, variables):
+def generate_tiny_prefix():
+    return uuid.uuid4().hex[0:12]
 
+
+def construct_playbook(yaml_file, role_name, variables):
     tasks = [
         {
             "name": "Create temporary directory to run test.",
             "register": "tmp_path",
-            "ansible.builtin.tempfile":{
-                "suffix": ".tf", "state": "directory"
-            }
+            "ansible.builtin.tempfile": {"suffix": ".tf", "state": "directory"},
         },
         {
             "name": f"Execute ansible role '{role_name}'",
             "vars": {"output_dir": "{{ tmp_path.path }}"},
-            "block": [
-                {
-                    "ansible.builtin.include_role": {
-                        "name": role_name
-                    }
-                }
-            ],
-            "always":[
+            "block": [{"ansible.builtin.include_role": {"name": role_name}}],
+            "always": [
                 {
                     "name": "Delete temporary directory",
                     "ansible.builtin.file": {
                         "state": "absent",
-                        "path": "{{ tmp_path.path }}"
-                    }
+                        "path": "{{ tmp_path.path }}",
+                    },
                 }
-            ]
+            ],
         },
     ]
 
@@ -63,14 +60,8 @@ def construct_playbook(yaml_file, role_name, variables):
     yaml.dump([data], yaml_file, default_flow_style=False, explicit_start=True)
 
 
-def skip_target(path, targets) -> bool:
-    
+def skip_target(path, targets, allow_slow) -> bool:
     if targets and path.stem not in targets:
-        return True
-    
-    runme = path / PosixPath("runme.sh")
-    if runme.exists():
-        print("[SKIP] ", path.stem, " contains 'runme.sh' file.")
         return True
 
     aliases = path / PosixPath("aliases")
@@ -78,20 +69,31 @@ def skip_target(path, targets) -> bool:
         return True
 
     attributes = aliases.read_text().split("\n")
-    if any(x.startswith("disabled") for x in attributes) or any(x.startswith("unsupported") for x in attributes):
+    if any(x.startswith("disabled") for x in attributes) or any(
+        x.startswith("unsupported") for x in attributes
+    ):
         print("[SKIP] ", path.stem, " disabled or unsupported")
-        return True 
+        return True
+
+    if allow_slow:
+        return False
 
     for x in attributes:
-        m = re.match('^time=([0-9]*)m', x)
+        m = re.match("^time=([0-9]*)m", x)
         if m:
             if int(m.group(1)) > 5:
-                print("[SKIP] ", path.stem, " too long, estimated time = ", m.group(1), "min")
+                print(
+                    "[SKIP] ",
+                    path.stem,
+                    " too long, estimated time = ",
+                    m.group(1),
+                    "min",
+                )
                 return True
     return False
 
 
-def run(eei_image, collection_path, vars_file, targets, use_stdout):
+def run(eei_image, collection_path, vars_file, targets, use_stdout, allow_slow):
     variables = {}
     if vars_file:
         with open(vars_file, "r") as fd:
@@ -117,34 +119,68 @@ def run(eei_image, collection_path, vars_file, targets, use_stdout):
     path = PosixPath(collection_path) / PosixPath("tests/integration/targets")
     results = []
     for target in path.iterdir():
-
-        if skip_target(target, targets):
+        if skip_target(target, targets, allow_slow):
             continue
 
         yaml_file = NamedTemporaryFile(mode="w", suffix=".yaml", delete=False)
         test_variables = copy.deepcopy(variables)
-        test_variables.update({"resource_prefix": generate_resource_prefix()})
-        construct_playbook(yaml_file, target.stem, test_variables)
-        yaml_file.close()
+        test_variables.update(
+            {
+                "resource_prefix": generate_resource_prefix(),
+                "tiny_prefix": generate_tiny_prefix(),
+                "ansible_test": {
+                    "environment": {
+                        "ANSIBLE_DEBUG_BOTOCORE_LOGS": "True"
+                    },
+                    "module_defaults": None,
+                },
+            }
+        )
 
-        command = [
-            "ansible-navigator",
-            "run",
-            yaml_file.name,
-            "-v",
-            "--ee true",
-            f"--eei {eei_image}",
-            "--ce docker",
-            f"--eev {path.expanduser()}:/roles:Z",
-            "-m stdout",
-        ] + set_envs
+        command = []
+        runme = target / PosixPath("runme.sh")
+        if runme.exists():
+            yaml.dump(test_variables, yaml_file, default_flow_style=False, explicit_start=True)
+            command = [
+                "docker",
+                "run",
+                "-v",
+                f"{str(target)}:/test",
+                "-v",
+                f"{os.path.dirname(yaml_file.name)}:/vars",
+                "-w",
+                "/test",
+                eei_image,
+                f"./runme.sh -e '@/vars/{os.path.basename(yaml_file.name)}'"
+            ]
+        else:
+            construct_playbook(yaml_file, target.stem, test_variables)
+            command = [
+                "ansible-navigator",
+                "run",
+                yaml_file.name,
+                "-v",
+                "--ee true",
+                f"--eei {eei_image}",
+                "--ce docker",
+                f"--eev {path.expanduser()}:/roles:Z",
+                "-m stdout",
+            ] + set_envs
+        yaml_file.close()
+        command = " ".join(command)
         print("\033[93m++++++++++++++++++++++++++++++++++++++++++++++++++")
         print(f"+    RUNNING TARGET => {target.stem}")
+        print(f"+ {command}")
         print("++++++++++++++++++++++++++++++++++++++++++++++++++\033[00m")
         if use_stdout:
-            cmd = subprocess.Popen(" ".join(command), shell=True)
+            cmd = subprocess.Popen(command, shell=True)
         else:
-            cmd = subprocess.Popen(" ".join(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            cmd = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+            )
         stdout, stderr = cmd.communicate()
         os.remove(yaml_file.name)
         result = f"++++ {target.stem} -> "
@@ -177,7 +213,12 @@ def main() -> None:
         "--vars-file", help="Path to a file containing variables to set to run tests."
     )
     parser.add_argument("--targets", help="Comma-separated list of targets to test.")
-    parser.add_argument("--use-stdout", action='store_true', help="Use stdout for the execution output")
+    parser.add_argument(
+        "--use-stdout", action="store_true", help="Use stdout for the execution output"
+    )
+    parser.add_argument(
+        "--allow-slow", action="store_true", help="Allow running too long targets"
+    )
 
     args = parser.parse_args()
 
@@ -187,6 +228,7 @@ def main() -> None:
         vars_file=args.vars_file,
         targets=args.targets,
         use_stdout=args.use_stdout,
+        allow_slow=args.allow_slow,
     )
 
 
